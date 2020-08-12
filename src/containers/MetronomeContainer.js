b44e7723b4ae6ee1c01155867be825fa82467edf
ed-commits <edward.alexander.cant@gmail.com>
2020-08-12 14:27:25 +0100

    time signatures

--- src/containers/MetronomeContainer.js
+++ src/containers/MetronomeContainer.js
@@ -8,11 +8,13 @@ class MetronomeContainer extends Component {
         this.state = {
             soundFiles: {
                 beat: new Audio("/sounds/metro_beat.ogg"),
-                claves: new Audio("/sounds/Metronom Claves.ogg")
+                clave: new Audio("/sounds/Metronom Claves.ogg")
             },
             intervalTime: 1,
             beatLight: false,
-            claveLight: false
+            claveLight: false,
+            timeSignature: "4/4",
+            timeSignatureCounter: 0
         }
     }
 
@@ -20,15 +22,42 @@ class MetronomeContainer extends Component {
         this.setState({ beatLight: false, claveLight: false });
     }
 
-    tick = () => {
+    beat = () => {
         this.state.soundFiles["beat"].play();
         this.setState({ beatLight: true });
-        setTimeout(this.switchOff, 1000*0.2);
     }
-    
+
+    clave = () => {
+        this.state.soundFiles["clave"].play();
+        this.setState({ claveLight: true });
+    }
+
+    tick = () => {
+        let timeSignatureCounter = (this.state.timeSignatureCounter + 1) % 4;
+        this.setState({ timeSignatureCounter: timeSignatureCounter });
+
+        if (this.state.timeSignature == "4/4") {
+            this.beat();
+        }
+        if (this.state.timeSignature == "2/4") {
+            if (timeSignatureCounter == 1 || timeSignatureCounter == 3) {
+                this.beat();
+            }
+            else {
+                this.clave();
+            }
+        }
+
+        setTimeout(this.switchOff, 1000 * 0.2);
+    }
+
     startTimer = () => {
         this.tick();
-        setInterval(this.tick, this.state.intervalTime*1000);
+        setInterval(this.tick, this.state.intervalTime * 1000);
+    }
+
+    timeSignature = (event) => {
+        this.setState({ timeSignature: event.target.value });
     }
 
     render = () => {
@@ -37,10 +66,13 @@ class MetronomeContainer extends Component {
                 <PlaySound id="beat" circleLightSwitch={this.state.beatLight} />
                 <PlaySound id="claves" circleLightSwitch={this.state.claveLight} />
                 <button onClick={this.startTimer}>Play</button>
+                <select onChange={this.timeSignature}>
+                    <option value="4/4">4/4</option>
+                    <option value="2/4">2/4</option>
+                </select>
             </>
         )
     }
-
 }
 
 export default MetronomeContainer;
